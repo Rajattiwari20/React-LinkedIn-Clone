3b49c812a1c7441ca798dd96fff399dc900240ec
Rajat Tiwari <rajat@truelancer.com>
2021-10-15 00:45:31 +0530

    Added Login Component

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Login = (props) =>{
+    return (
+        <>  
+            <h1>LogIn</h1>
+        </>
+    )
+        
+
+}
+
+export default Login;
